b47e3e53103315aeb79c304c056f365cbbbb8669
lifeae <romain.leporatti@laposte.net>
2020-03-09 16:46:00 +0100

    error NameError: uninitialized constant Categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,17 +2,54 @@ require 'faker'
 # Fichier à màj avec les nouvelles tables et les fakers souhaités :
 # Lien vers faker : https://github.com/faker-ruby/faker
 
-puts "Réinitialisation de la table Table1"
-Table1.destroy_all
-puts "Table Table1 réinitialisée"
+puts "Réinitialisation des tables ..."
+Categories.destroy_all
+print "Categories"
+Items.destroy_all
+print ", Items"
+Purchases.destroy_all
+print ", Purchases"
+Users.destroy_all
+puts ", Users."
+puts "Tables réinitialisées"
 
 puts "Création des seeds ..."
 seed = 1
 5.times do
-  Table1.create(
-    object: Faker::Lorem.words,
-    body: Faker::Lorem.sentence)
-    puts "Création du seed #{seed}"
+  Categories.create(
+    name: Faker::Lorem.words)
+    puts "Seed Categories n°#{seed} crée."
+    seed += 1
+end
+seed = 1
+image_url = 1
+5.times do
+  Items.create(
+    title: Faker::Lorem.words,
+    description: Faker::Lorem.sentence,
+    price: Faker::Commerce.price,
+    image_url: "#{image_url}.jpg",
+    category_id:2,)
+    puts "Seed Items n°#{seed} crée."
+    seed += 1
+    image_url += 1
+end
+seed = 1
+5.times do
+  Purchases.create(
+    user_id: 2,
+    item_id: 2,
+    quantity: 1,
+    unite_price: Faker::Commerce.price,
+    stripe_id: Faker::Alphanumeric.alpha(number: 10))
+    puts "Seed Purchases n°#{seed} crée."
+    seed += 1
+end
+seed = 1
+5.times do
+  Users.create(
+    email: Faker::Lorem.words)
+    puts "Seed Users n°#{seed} crée."
     seed += 1
 end
 puts "Création des seeds terminées."
