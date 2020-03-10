4fd38c848648b9c8f3649f1d4b4db494f0e16f64
Akushon <alann.deleau@gmail.com>
2020-03-10 11:55:32 +0100

    mise des partial dans views/shared, le root est maintenant dans items#index et supression de la partial head mise directement dans application

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root 'items#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
-  resources :items, only: [:show]
+  resources :items, only: [:show, :index]
 end
