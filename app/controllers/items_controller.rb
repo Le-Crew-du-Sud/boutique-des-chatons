16a75bdf8e64c2281a5415aae3d6e4ffbe93f89a
lifeae <romain.leporatti@laposte.net>
2020-03-10 15:54:05 +0100

    add new def as of Eventbrite

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,31 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+
+  def create
+    puts params[:title], params[:description]
+    @create_item = Item.new(title: params[:title], description: params[:description], price: params[:price],image_url: params[:price])
+    if @create_item.save
+      puts "Un produit a été créé"
+      flash[:notice] = "Un produit a été créé."
+      redirect_to items_path
+    else
+      puts "Erreur dans l'enregistrement.Ton produit a été refusé"
+      flash[:alert] = "Erreur dans l'enregistrement.Ton produit a été refusé"
+      render 'new'
+    end
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to index_path
+  end
 end
+
