71ec3c00e3f085de028a8062d3e4b6d33937e242
fbrgmn <frbergamini@gmail.com>
2020-03-10 17:08:33 +0100

    Setup of ActionMailer Development is using "letter opener" Production is configured to use "sendgrid"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ApplicationRecord
+  after_create :welcome_send
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :purchases
-  
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 end
