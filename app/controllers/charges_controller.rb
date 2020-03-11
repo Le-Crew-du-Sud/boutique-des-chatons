2e4b8859a1dd6a6eb49ddc8fcb6ac387cd0cb9d2
fbrgmn <frbergamini@gmail.com>
2020-03-11 11:57:22 +0100

    a la creation du controller test de Stripe rails g controller charges

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,5 @@
+class ChargesController < ApplicationController
+  # ce controller a été crée pour tester Stripe. Il doit être effacé aprés integration
+  # avec la boutique
+  # rails d controller charges # pour l'effacer
+end
