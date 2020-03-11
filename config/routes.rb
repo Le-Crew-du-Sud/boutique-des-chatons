290e1a62722b8b4b9fc5ad6e94b5efec212f645d
Maxime Besse <bessemaxime@gmail.com>
2020-03-11 15:29:19 +0100

    set routes for the controller categories

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :items
+  resources :categories, only: [:show]
   resources :users, only: [:edit, :show]
 end
