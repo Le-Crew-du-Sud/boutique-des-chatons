db85d488ca4333319a2b953d970fc755db4e997f
fbrgmn <frbergamini@gmail.com>
2020-03-11 12:11:38 +0100

    stripe integré et functionel. il faudra l'integrer avec la boutique au moment donné

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -2,4 +2,28 @@ class ChargesController < ApplicationController
   # ce controller a été crée pour tester Stripe. Il doit être effacé aprés integration
   # avec la boutique
   # rails d controller charges # pour l'effacer
+
+  def new
+  end
+
+  def create
+    # Amount in cents
+    @amount = 500
+
+    customer = Stripe::Customer.create({
+      email: params[:stripeEmail],
+      source: params[:stripeToken],
+    })
+
+    charge = Stripe::Charge.create({
+      customer: customer.id,
+      amount: @amount,
+      description: 'Rails Stripe customer',
+      currency: 'usd',
+    })
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_charge_path
+  end
 end
