7b8db607cb8cd3cdc7c387fbbb21a0b9734d08f6
Maxime Besse <bessemaxime@gmail.com>
2020-03-13 10:51:07 +0100

    Fixed problems about category when not authenticate

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,16 @@
+class CategoriesController < ApplicationController
+
+
+  def index
+    redirect_to index_path
+  end
+
+  def show
+    @categorie = Category.find(params[:id])
+
+    respond_to do |format|
+      format.html { redirect_to "/" }
+      format.js {}
+    end
+  end
+end
